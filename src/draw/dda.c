c66ea6efb4d7b5c9ef46d33d452c8204bcfa953f
itakumi-42tokyo <tigarashi@LAPTOP-MOSC4HI6.localdomain>
2025-07-16 22:56:50 +0900

    dda.c

--- src/draw/dda.c
+++ src/draw/dda.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   dda.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: itakumi <itakumi@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: tigarashi <tigarashi@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 23:08:39 by itakumi           #+#    #+#             */
-/*   Updated: 2025/07/13 20:22:40 by itakumi          ###   ########.fr       */
+/*   Updated: 2025/07/16 22:51:17 by tigarashi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,53 @@
 // ２．ラスタライズする
 // ３．1 < 1 > で場合分けする
 
-int	dda()
-{
+#include "../../lib/minilibx-linux/mlx.h"
+#include <math.h>
+#include <stdlib.h>
 
+int	my_abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
+
+void	dda(int x1, int y1, int x2, int y2, void *mlx, void *win)
+{
+	int dx = x2 - x1;
+	int dy = y2 - y1;
+	int steps;
+	double x_inc, y_inc;
+	double x = x1;
+	double y = y1;
+	int i;
+
+	steps = (my_abs(dx) > my_abs(dy)) ? my_abs(dx) : my_abs(dy);
+	x_inc = dx / (double)steps;
+	y_inc = dy / (double)steps;
+	for (i = 0; i <= steps; i++)
+	{
+		mlx_pixel_put(mlx, win, round(x), round(y), 0xFFFFFF);
+		x += x_inc;
+		y += y_inc;
+	}
+}
+
+int	expose_hook(void *param)
+{
+	void **p = (void **)param;
+	void *mlx = p[0];
+	void *win = p[1];
+	dda(0, 900, 900, 0, mlx, win);
+	return (0);
+}
+
+int	main(void)
+{
+	void *mlx = mlx_init();
+	void *win = mlx_new_window(mlx, 900, 900, "Hello, World!");
+	void *param[2] = {mlx, win};
+	mlx_expose_hook(win, expose_hook, param);
+	mlx_loop(mlx);
+	return (0);
 }
