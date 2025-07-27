fbda701d993147caf4f704ea739750aa71c4d1f0
Igarashi Takumi <itakumi@c6r3s10.42tokyo.jp>
2025-07-27 21:11:50 +0900

    no leak

--- src/exit/free_exit.c
+++ src/exit/free_exit.c
@@ -6,7 +6,7 @@
 /*   By: itakumi <itakumi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 19:13:23 by itakumi           #+#    #+#             */
-/*   Updated: 2025/07/27 16:30:07 by itakumi          ###   ########.fr       */
+/*   Updated: 2025/07/27 21:11:06 by itakumi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include "mlx.h"
 #include "libft.h"
 #include "struct.h"
+#include "utils.h"
 
 void	free_exit(t_control **ctrl)
 {
@@ -24,13 +25,13 @@ void	free_exit(t_control **ctrl)
 		(ft_putstr_fd("invalid argument: ctrl", 2), exit(EXIT_FAILURE));
 	if ((*ctrl)->img != NULL)
 		mlx_destroy_image((*ctrl)->mlx, (*ctrl)->img);
-	if ((*ctrl)->win != NULL)
-		mlx_destroy_window((*ctrl)->mlx, (*ctrl)->win);
-	if ((*ctrl)->mlx != NULL)
-	{
-		mlx_loop_end((*ctrl)->mlx);
-		mlx_destroy_display((*ctrl)->mlx);
-	}
+	mlx_destroy_window((*ctrl)->mlx, (*ctrl)->win);
+	mlx_loop_end((*ctrl)->mlx);
+	mlx_destroy_display((*ctrl)->mlx);
 	free((*ctrl)->mlx);
+	free_2d((void **)((*ctrl)->map));
+	free_2d((void **)((*ctrl)->iso_map));
+	free_2d((void **)((*ctrl)->persp_map));
+	free((*ctrl));
 	exit(EXIT_SUCCESS);
 }
