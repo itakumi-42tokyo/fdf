2de647d7bfff40e43c5203edbc95a7ba047998a0
Igarashi Takumi <itakumi@c5r1s7.42tokyo.jp>
2025-08-11 16:04:25 +0900

    proj

--- includes/projection.h
+++ includes/projection.h
@@ -6,7 +6,7 @@
 /*   By: itakumi <itakumi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 17:08:01 by itakumi           #+#    #+#             */
-/*   Updated: 2025/08/08 15:15:04 by itakumi          ###   ########.fr       */
+/*   Updated: 2025/08/11 15:53:58 by itakumi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include "struct.h"
 
+int		apply_persp_to_matrix(double marix[4][4]);
 int		apply_iso_to_matrix(double matrix[4][4]);
 int		iso_proj(t_control *ctrl);
 int		apply_persp_to_matrix(double matrix[4][4]);
--- src/pipeline/projection/perspective.c
+++ src/pipeline/projection/perspective.c
@@ -6,7 +6,7 @@
 /*   By: itakumi <itakumi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 17:09:46 by itakumi           #+#    #+#             */
-/*   Updated: 2025/08/07 13:25:10 by itakumi          ###   ########.fr       */
+/*   Updated: 2025/08/11 16:02:27 by itakumi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -160,6 +160,14 @@
 // }
 
 // take 2
+
+int	apply_persp_to_matrix(double marix[4][4])
+{
+	// const double	matrix_persp =
+	// {
+	// };
+	return (0);
+}
 
 int	persp_proj(t_control *ctrl)
 {
