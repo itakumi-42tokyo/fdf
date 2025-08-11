275098312ddfc2d866cd471a7dd23f7eb17ea063
Igarashi Takumi <itakumi@c5r1s7.42tokyo.jp>
2025-08-11 21:06:56 +0900

    norm: auto_fit_scale

--- src/pipeline/view/auto_fit_scale.c
+++ src/pipeline/view/auto_fit_scale.c
@@ -6,34 +6,34 @@
 /*   By: itakumi <itakumi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 21:34:39 by tigarashi         #+#    #+#             */
-/*   Updated: 2025/08/07 08:08:56 by itakumi          ###   ########.fr       */
+/*   Updated: 2025/08/11 20:46:00 by itakumi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <limits.h>
-#include <math.h>
 #include "libft.h"
+#include "macro.h"
 #include "struct.h"
 #include "view.h"
-#include "macro.h"
+#include <limits.h>
+#include <math.h>
+#include <unistd.h>
 
 // zはスケーリングしなくてよいのか。
-static void calc_bounds_local(t_control *ctrl, int min_xy[2], int max_xy[2])
+static void	calc_bounds_local(t_control *ctrl, int min_xy[2], int max_xy[2])
 {
-    int i;
+	int	i;
 	int	j;
 	int	ix;
-	int iy;
+	int	iy;
 
-	min_xy[0] = min_xy[1] =  INT_MAX;
-    max_xy[0] = max_xy[1] = -INT_MAX;
+	min_xy[0] = min_xy[1] = INT_MAX;
+	max_xy[0] = max_xy[1] = -INT_MAX;
 	i = 0;
-    while (i < ctrl->map_height)
+	while (i < ctrl->map_height)
 	{
 		j = 0;
-        while (j < ctrl->map_width)
-        {
+		while (j < ctrl->map_width)
+		{
 			if (PROJ == ISO)
 			{
 				ix = ctrl->iso_map[i][j].iso_x;
@@ -45,43 +45,43 @@ static void calc_bounds_local(t_control *ctrl, int min_xy[2], int max_xy[2])
 				iy = ctrl->persp_map[i][j].persp_y;
 			}
 			else
-			{
 				ft_putstr_fd(ERROR_PROJ_MACRO, STDERR_FILENO);
-				// exit するのかどうか
-			}
-            if (ix < min_xy[0])
+			if (ix < min_xy[0])
 				min_xy[0] = ix;
-            if (iy < min_xy[1])
+			if (iy < min_xy[1])
 				min_xy[1] = iy;
-            if (ix > max_xy[0])
+			if (ix > max_xy[0])
 				max_xy[0] = ix;
-            if (iy > max_xy[1])
+			if (iy > max_xy[1])
 				max_xy[1] = iy;
 			j++;
-        }
+		}
 		i++;
 	}
 }
 
+// 最終的に min_xy もゼロ原点に移してから中央に配置
 // 拡大縮小のオーバーフロー処理をしないといけない。
-void auto_fit_scale(t_control *ctrl, float fit_ratio)
+void	auto_fit_scale(t_control *ctrl, float fit_ratio)
 {
-    int min_xy[2];
-	int	max_xy[2];
-    double w;
-    double h;
+	int		min_xy[2];
+	int		max_xy[2];
+	double	w;
+	double	h;
+	double	sx_sy[2];
 
 	calc_bounds_local(ctrl, min_xy, max_xy);
 	w = max_xy[0] - min_xy[0];
 	h = max_xy[1] - min_xy[1];
-    if (w <= 0)
+	if (w <= 0)
 		w = 1;
-    if (h <= 0)
+	if (h <= 0)
 		h = 1;
-    double sx = (ctrl->win_size_x * (double)fit_ratio) / w;
-    double sy = (ctrl->win_size_y * (double)fit_ratio) / h;
-    ctrl->scale = fmin(sx, sy);
-    // 最終的に min_xy もゼロ原点に移してから中央に配置
-    ctrl->offset_x = (ctrl->win_size_x - ctrl->scale * w) / 2 - min_xy[0] * ctrl->scale;
-    ctrl->offset_y = (ctrl->win_size_y - ctrl->scale * h) / 2 - min_xy[1] * ctrl->scale;
+	sx_sy[0] = (ctrl->win_size_x * (double)fit_ratio) / w;
+	sx_sy[1] = (ctrl->win_size_y * (double)fit_ratio) / h;
+	ctrl->scale = fmin(sx_sy[0], sx_sy[1]);
+	ctrl->offset_x = (ctrl->win_size_x - ctrl->scale * w) / 2 - min_xy[0]
+		* ctrl->scale;
+	ctrl->offset_y = (ctrl->win_size_y - ctrl->scale * h) / 2 - min_xy[1]
+		* ctrl->scale;
 }
